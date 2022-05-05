045468ddb2b0d02f912951244c7e6d7edc6f4550
clx <1149302926@qq.com>
2022-05-05 15:04:50 +0800

    搭建html结构

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
-import frank from "./components/frank.vue";
-import frank2 from "./components/frank2.vue";
+import Doc from "./views/Doc.vue";
+import Home from "./views/Home.vue";
 import { createWebHashHistory, createRouter } from "vue-router";
 
 const history = createWebHashHistory();
 const router = createRouter({
   history: history,
   routes: [
-    { path: "/", component: frank },
-    { path: "/xxx", component: frank2 },
+    { path: "/", component: Home },
+    { path: "/doc", component: Doc },
   ],
 });
 const app = createApp(App);
