170a94ee7c69c914dd8539361dff2d421d0fd4a6
clx <1149302926@qq.com>
2022-05-05 15:56:07 +0800

    初始化aside的html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,11 +1,24 @@
 <template>
-  <Topnav />
-  <div class="topnav">
-    <div class="logo"></div>
-    <div class="menu"></div>
-  </div>
+  <Topnav class="nav" />
+
   <div class="content">
-    <aside>边栏</aside>
+    <aside>
+      <h2>组件列表</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/switch">Switch 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs 组件</router-link>
+        </li>
+      </ol>
+    </aside>
     <main>主内容</main>
   </div>
 </template>
