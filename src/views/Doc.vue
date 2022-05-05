f2bba10498373630623797953decc2928f9619bc
clx <1149302926@qq.com>
2022-05-05 18:05:27 +0800

    基本完成官网框架

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,25 +1,28 @@
 <template>
-  <Topnav class="nav" />
-
-  <div class="content">
-    <aside v-if="menuVisible">
-      <h2>组件列表</h2>
-      <ol>
-        <li>
-          <router-link to="/doc/switch">Switch 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/button">Button 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/dialog">Dialog 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/tabs">Tabs 组件</router-link>
-        </li>
-      </ol>
-    </aside>
-    <main><router-view /></main>
+  <div class="layout">
+    <Topnav class="nav" />
+    <div class="content">
+      <aside v-if="menuVisible">
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
@@ -37,6 +40,7 @@ export default {
 </script>
 
 
+</script>
 <style lang="scss" scoped>
 .layout {
   display: flex;
@@ -47,7 +51,7 @@ export default {
   }
   > .content {
     flex-grow: 1;
-    padding-top: 60px;
+    padding-top: 5px;
     padding-left: 156px;
     @media (max-width: 500px) {
       padding-left: 0;
@@ -69,7 +73,10 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
   height: 100%;
   > h2 {
     margin-bottom: 4px;
@@ -79,14 +86,7 @@ aside {
       padding: 4px 0;
     }
   }
-  @media (max-width: 500px) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
-  }
 }
-
 main {
   overflow: auto;
 }
