b4297324cb08e138a421a1954e4d89815a83c546
clx <1149302926@qq.com>
2022-05-06 19:04:28 +0800

    Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>button</div>
+  <div>
+    <div>button</div>
+    <h1>button示例</h1>
+    <div><Button>你好</Button></div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+};
 </script>
 
 <style lang="scss" scoped>
