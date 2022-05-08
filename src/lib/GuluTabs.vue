8e44322860eac9409de8d13cc259e9917e8b6e03
clx <1149302926@qq.com>
2022-05-08 17:09:41 +0800

    切换内容和标题

--- src/lib/GuluTabs.vue
+++ src/lib/GuluTabs.vue
@@ -1,19 +1,52 @@
+
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :key="index" :is="c" />
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div
+        class="gulu-tabs-nav-item"
+        :class="{ selected: t === selected }"
+        @click="select(t)"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component
+        class="gulu-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :class="{ selected: c.props.title === selected }"
+        :key="index"
+        :is="c"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from "./GuluTab.vue";
+import { computed } from "vue";
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error("tabs 子标签必须是Tab");
       }
+    });
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
     });
     const titles = defaults.map((tag) => {
       return tag.props.title;
@@ -22,6 +55,8 @@ export default {
     return {
       defaults,
       titles,
+      current,
+      select,
     };
   },
   components: { Tab },
@@ -29,4 +64,44 @@ export default {
 </script>
 
 <style lang="scss" >
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
 </style>
