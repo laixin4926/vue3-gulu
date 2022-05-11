5513dd50282abf37cbadefeeef4f7a34dc35338f
clx <1149302926@qq.com>
2022-05-11 16:26:59 +0800

    优化代码

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import "./index.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "github-markdown-css";
+import Markdown from "./components/Markdown.vue";
 
 import { router } from "./router";
 
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+app.component("Markdown", Markdown);
