136afe3642bea9093e858111a03f308a7d0e87ae
clx <1149302926@qq.com>
2022-05-11 19:29:07 +0800

    报错

--- src/components/Switch1.demo.vue
+++ src/components/Switch1.demo.vue
@@ -1,18 +1,50 @@
+<demo>
+常规使用
+</demo>
 <template>
-  <Switch v-model:value="bool" />
+  <div>
+    <Button @click="toggle">打开对话框</Button>
+    <Dialog
+      v-model:visible="x"
+      :closeOnClickOverlay="false"
+      :ok="f1"
+      :cancel="f2"
+    >
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi2</div>
+      </template>
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+    </Dialog>
+  </div>
 </template>
 
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
 import { ref } from "vue";
+import { openDialog } from "../lib/openDialog";
 export default {
   components: {
-    Switch,
+    Dialog,
+    Button,
   },
   setup() {
-    const bool = ref(false);
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
     return {
-      bool,
+      x,
+      toggle,
+      f1,
+      f2,
     };
   },
 };
