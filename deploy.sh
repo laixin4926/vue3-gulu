b16d755d609cb365e7023f9c70ce0fc0c3515e97
clx <1149302926@qq.com>
2022-05-14 18:22:20 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,4 @@ git commit -m "update" &&
 git branch -M master &&
 git remote add origin git@github.com:laixin4926/vue3-gulu-webpack.git
 git push -f -u origin master &&
-cd - &&
-git init &&
-git add . &&
-git commit -m "update" &&
-git branch -M main &&
-git remote add origin git@github.com:laixin4926/vue3-gulu.git
-git push -f -u origin master &&
+cd - 
