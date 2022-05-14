2e59ae30505fe5dca7b874ff96524c0f24907cfe
clx <1149302926@qq.com>
2022-05-14 16:56:03 +0800

    一键部署

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@github.com:laixin4926/vue3-gulu-webpack.git
+git push -f -u origin master &&
+cd -
+echo https://frankfang.gitee.io/gulu-ui-website-1/
